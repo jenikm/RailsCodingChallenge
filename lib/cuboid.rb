e08383b76791ba4a64e16b684cad24efa92bef26
Eugene Magdel <jenikm@gmail.com>
2015-08-09 15:29:51 -0500

    Implementing cuboid with tests
    Important methods:
      move_to!
      rotate!
      vertices
      intersects?

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,17 +1,132 @@
 
 class Cuboid
-  
-  #BEGIN public methods that should be your starting point
 
-  def move_to!(x, y, z)
+  attr_accessor :origin, :dimensions
+
+  # @arguments [Integer,Integer,Integer,Integer,Integer,Integer] - x,y,z, l,h,w
+  def initialize(*origin_and_dimensions)
+    origin = (self.class.coordinates_to_hash self.class.dimension_names, origin_and_dimensions[0,3])
+    dimensions = (self.class.coordinates_to_hash self.class.dimension_names, origin_and_dimensions[3,3])
+    vertices = (self.class.vertices (self.class.origin_to_center origin, dimensions), dimensions)
+
+    if (self.class.vertices_valid? vertices)
+      self.origin = origin
+      self.dimensions = dimensions
+    else
+      throw ArgumentError, "Invalid cuboid, one of the vertices is out of bound: #{ vertices.inspect }"
+    end
   end
-  
+
+  # @arguments [Integer,Integer,Integer] - x,y,z
+  # Moves object to the new origin
+  # If movement was successful, returns true
+  # Does not change state if update failed, returns false
+  def move_to!(*origin)
+    origin = (self.class.coordinates_to_hash self.class.dimension_names, origin)
+    vertices = (self.class.vertices (self.class.origin_to_center origin, dimensions), dimensions)
+    if (self.class.vertices_valid? vertices)
+      self.origin = origin
+      true
+    else
+      false
+    end
+  end
+
+  # @arguments [Symbol,Symbol] 2d plane (Ex :x,:y)
+  # @return Boolean
+  # If rotation is successful, return true
+  # Does not change state if update failed, returns false
+  def rotate!(*plane)
+    dimensions = (self.class.swap_dimension self.dimensions,*plane)
+    vertices = (self.class.vertices (self.class.origin_to_center origin, dimensions), dimensions)
+    if (self.class.vertices_valid? vertices)
+      self.dimensions = dimensions
+      self.origin = dimensions.first
+      true
+    else
+      false
+    end
+  end
+
+  # Returns 8 vertices with first vertex as the smallest and last vertex the largest
+  # @returns Array[ Hash ]
   def vertices
-  end
-  
-  #returns true if the two cuboids intersect each other.  False otherwise.
-  def intersects?(other)
+    (self.class.vertices (self.class.origin_to_center origin, dimensions), dimensions)
   end
 
-  #END public methods that should be your starting point  
+  # @arguemnt [Cuboid]
+  # @return true if the two cuboids intersect each other.  False otherwise.
+  # Cuboids interect IFF for every dimension, there is an interesction
+  def intersects?(other)
+    self.class.dimension_names.all? do |dimension_name|
+      (self.class.dimension_interesects? vertices.last, other.vertices.first, dimension_name)
+    end
+  end
+
+  class << self
+
+    def half(v)
+      v / 2
+    end
+
+    def dimension_names
+      [:x,:y,:z]
+    end
+
+    # @arguments [Hash,Hash,Hash]
+    # @return [Hash]
+    def build_vertex(center, dimensions, unit_vertex )
+      dimension_names.reduce({}) do |a, dimension|
+        a.merge(
+          { dimension =>
+            center[dimension] + (unit_vertex[dimension] * (half dimensions[dimension]))})
+      end
+    end
+
+    # @arguemnt [Integer] - vertex number
+    # @return [Hash] - vertex with all coordinates set to 1 or -1 based on input specified
+    # Used internally to calculate vertices, but subtracting or adding half of dimension
+    def build_unit_vertex(i)
+      coordinates_to_hash dimension_names, [i / 4, i / 2, i].map{|v| v.even? ? -1 : 1}
+    end
+
+    def dimension_interesects?(max_vertex_a, min_vertex_b, dimension)
+      max_vertex_a[dimension] >= min_vertex_b[dimension]
+    end
+
+    def coordinates_to_hash(names, values)
+      (names.zip values).reduce({}){|a,v| (a.merge Hash[*v])}
+    end
+
+    def swap_dimension(dimensions, axis1, axis2)
+      dimensions.merge({ axis1 => dimensions[axis2], axis2 => dimensions[axis1]} )
+    end
+
+    def vertices(center, dimensions, v_number=7)
+      if v_number < 0
+        []
+      else
+        (vertices center, dimensions, v_number - 1).concat [
+          (build_vertex center, dimensions, (build_unit_vertex v_number)) ]
+      end
+    end
+
+
+    # @arguments [Hash,Hash] - center,dimensions
+    # @return [Boolean]
+    # Returns true if smallest vertex has positive values
+    def vertices_valid?(vertices)
+      vertices.first.values.min >= 0
+    end
+
+    # @arguemnts [Hash,Hash]
+    # @return [Hash]
+    # Figures out center of cuboid, simplifies formula
+    def origin_to_center(origin, dimensions)
+      dimension_names.reduce({}) do |all,dimension_name|
+        all.merge({dimension_name => (origin[dimension_name] + (half dimensions[dimension_name]) )})
+      end
+    end
+  end
+
 end
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,67 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
-#  Do whatever you need to do to make it work and please add your own test cases for as many
-#  methods as you feel need coverage
 describe Cuboid do
- 
-  describe "move_to" do
+  let(:subject_origin){[5,5,5]}
+  let(:subject_dimensions){[10,10,10]}
+
+  subject {Cuboid.new(*subject_origin.concat(subject_dimensions))}
+  let(:rectangular){Cuboid.new(100,5,5,10,10,10)}
+
+  describe "move_to!" do
     it "changes the origin in the simple happy case" do
       expect(subject.move_to!(1,2,3)).to be true
     end
-  end    
-  
+  end
+
   describe "intersects?" do
+
+    let(:next_to_subject) {Cuboid.new(16,16,16,10,10,10)}
+    let(:intersects_subject_with_one_vertex_shared) {Cuboid.new(15,15,15,10,10,10)}
+    let(:far_away_from_subject) {Cuboid.new(50,50,50,10,10,10)}
+
+    it "intersects self" do
+      (expect (subject.intersects? subject)).to be true
+    end
+
+    it "does not interesect cube that is far away" do
+      (expect (subject.intersects? far_away_from_subject)).to be false
+    end
+
+    it "does not interesect cube that is next to other cube" do
+      (expect (subject.intersects? next_to_subject)).to be false
+    end
+
+    it "Shares one vertex with subject and intersects" do
+      (expect (subject.vertices & intersects_subject_with_one_vertex_shared.vertices).length).to be 1
+      (expect subject.intersects? intersects_subject_with_one_vertex_shared).to be true
+    end
+  end
+
+  describe "vertices" do
+
+    let(:largest_vertex_dimensions) do
+      subject_dimensions.zip(subject_origin).map(&lambda{|x,y| x+y})
+    end
+
+    it "Should have 8 vertices" do
+      (expect subject.vertices.length).to eq 8
+    end
+
+    it "Largest vertex should have largest coordinates" do
+      largest_vertex = subject.vertices.last
+      (expect [largest_vertex[:x],largest_vertex[:y], largest_vertex[:z]]).to eq largest_vertex_dimensions
+    end
+  end
+
+  describe "rotate" do
+    let(:rotated_rectangular){ r=rectangular.clone;r.rotate!(:x,:y);r }
+
+    it "Should swap x,y dimensions" do
+      (expect rotated_rectangular.dimensions[:x]).to be rectangular.dimensions[:y]
+      (expect rotated_rectangular.dimensions[:y]).to be rectangular.dimensions[:x]
+      (expect rotated_rectangular.dimensions[:z]).to be rectangular.dimensions[:z]
+    end
+
   end
 
 end
